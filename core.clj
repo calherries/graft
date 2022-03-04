dcb99902fe03322887aa343b62204ac8033e2930
Callum Herries <hi@callumherries.com>
2022-03-04 12:24:15 +0200

    Add transact function

--- core.clj
+++ core.clj
@@ -3,6 +3,14 @@
 (defn entity-ref [entity]
   (first (filter (fn [[k _]] (= (name k) "id")) entity)))
 
+(defn entity-ref? [x]
+  (and (vector? x)
+       (= (count x) 2)
+       (let [[id-key id-value] x]
+         (and (keyword? id-key)
+              (= (name id-key) "id"))
+         (int? id-value))))
+
 (defn shape [entity]
   (if (map? entity)
     (vec (for [[k v] entity]
@@ -16,57 +24,19 @@
     ;; else vector?
     (shape (first entity))))
 
-(comment
-  (def entity {:person/id   1
-               :person/name "hello"})
-  (entity-ref entity) ;; => [:person/id 1]
-  (shape entity) ;; => [:person/id :person/name]
-
-  (def entity {:person/id   1
-               :person/pet {:pet/name "Dotty"}})
-  (shape entity) ;; => [:person/id #:person{:pet [:pet/name]}]
-
-  (def entity {:person/id   1
-               :person/pets [{:pet/id   2
-                              :pet/name "Dotty"}
-                             {:pet/id 3
-                              :pet/name "Hello"}]})
-  (shape entity) ;; => [:person/id #:person{:pets [:pet/id :pet/name]}]
-  )
-
 ;; API
 
 (defn empty-db []
   {})
 
 (defn db-with [db entities]
-  (into {} (for [entity entities]
-             [(entity-ref entity) entity])))
+  (reduce
+   (fn [db entity]
+     (assoc db (entity-ref entity) entity)
+   {}
+   entities))
 
-(comment
-  (def entity {:person/id   1
-               :person/name "hello"})
-  (def db (db-with (empty-db) [{:person/id   1
-                                :person/name "Fred"
-                                :person/age  52
-                                :person/pets [[:animal/id 3]
-                                              [:animal/id 4]]}
-                               {:person/id   2
-                                :person/name "Dr. Rich"
-                                :person/age  25}
-                               {:person/id   2
-                                :person/name "Jessica"}
-                               {:animal/id   3
-                                :animal/name "Catso"
-                                :animal/vet  [[:person/id 2]]}
-                               {:animal/id   4
-                                :animal/name "Doggy"
-                                :animal/vet  [[:person/id 2]]}]))
 
-  {[:customer/id 123] [:customer/name :customer/email]}
-  )
-
-(defn p [x] (do (prn x) x))
 
 (defn q
   "Queries the database. Supports multiple joins"
@@ -86,6 +56,23 @@
                 [field (get entity field)]))))))
 
 (comment
+  (def db (db-with (empty-db) [{:person/id   1
+                                :person/name "Fred"
+                                :person/age  52
+                                :person/pets [[:animal/id 3]
+                                              [:animal/id 4]]}
+                               {:person/id   2
+                                :person/name "Dr. Rich"
+                                :person/age  25}
+                               {:person/id   2
+                                :person/name "Jessica"}
+                               {:animal/id   3
+                                :animal/name "Catso"
+                                :animal/vet  [[:person/id 2]]}
+                               {:animal/id   4
+                                :animal/name "Doggy"
+                                :animal/vet  [[:person/id 2]]}]))
+
   (q db {[:person/id 1] [:person/name :person/age]})
   (q db {[:animal/id 3] [:animal/name]})
   (q db {[:person/id 1] [:person/age
@@ -98,4 +85,28 @@
   (q db {[:person/id 1] [:person/age
                          {:person/pets [:animal/name
                                         {:animal/vet [:person/name]}]}]})
+  ;; => (#:person{:age 52,
+  ;;      :pets (#:animal{:name "Catso", :vet (#:person{:name "Jessica"})}
+  ;;                     #:animal{:name "Doggy", :vet (#:person{:name "Jessica"})})})
+
+  )
+
+(defn transact! [db transaction]
+  "Updates the database with data."
+  (reduce
+   (fn [db' [id operation-map]]
+     (reduce
+      (fn [db'' [operation args]]
+        (case operation
+          :add (update db'' id merge args)))
+      db'
+      operation-map))
+   db
+   transaction))
+
+(comment
+  (-> db
+      (transact! {[:person/id 1] {:add {:person/name "Freddy"}}})
+      (q {[:person/id 1] [:person/name]}))
+  ;; => (#:person{:name "Freddy"})
   )
